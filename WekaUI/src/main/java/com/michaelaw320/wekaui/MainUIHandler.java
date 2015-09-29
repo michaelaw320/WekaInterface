8286a515347a2b2a848b16784dc3b58b23efd2df
michaelaw320 <michaelaw320@gmail.com>
2015-09-29 22:06:39 +0700

    initial commit, openfile dialog for arff and csv

---
+++ WekaUI/src/main/java/com/michaelaw320/wekaui/MainUIHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2015 Michael
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.michaelaw320.wekaui;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.filters.Filter;
+
+/**
+ *
+ * @author Michael
+ */
+public class MainUIHandler {
+      /** the classifier used internally */
+  protected Classifier m_Classifier = null;
+  
+  /** the filter to use */
+  protected Filter m_Filter = null;
+
+  /** the training file */
+  protected String m_TrainingFile = null;
+
+  /** the training instances */
+  protected Instances m_Training = null;
+
+  /** for evaluating the classifier */
+  protected Evaluation m_Evaluation = null;
+
+    public MainUIHandler(){}
+    
+    public void openFile(String filename) {
+        m_TrainingFile = filename;
+        System.out.println(m_TrainingFile);
+    }
+}
